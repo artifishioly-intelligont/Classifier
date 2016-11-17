964215a7f029ee320484fe6926ef3729bfb78ea3
Tom Potter <tp10g13@soton.ac.uk>
2016-11-17 16:48:05 +0000

    Implemented learn and guess endpoints

--- service/App.py
+++ service/App.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 import AppReactions as react
-import SVM as SVM
 from copy import deepcopy
+import json
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def show_endpoints():
            '\t/guess/ -- Guess the class of a collection of vectors<br>'
 
 
-@app.route('/learn')
+@app.route('/learn', methods=['GET', 'POST'])
 def learn():
     print "{} /learn".format(request.method)
 
@@ -22,32 +22,72 @@ def learn():
         return react.learn_get()
         
     elif request.method == 'POST':
-        true_class = request.form['feature']
-        vector_dict = request.get_json['vectors']
+        true_class = float(request.get_json()['feature'])
+        vector_dict = request.get_json()['vectors']
+        
+        print len(vector_dict)
         
         attr_vecs = []
         true_classes = []
         for url in vector_dict.keys():
-            attr_vec = deepcopy(vector_dict[url])
+            attr_vec = deepcopy([float(i) for i in vector_dict[url]])
             attr_vecs.append(attr_vec)
             true_classes.append(true_class)
         
-        return react.learn_post(attr_vecs, true_classes)
+        react.learn_post(attr_vecs, true_classes)
+        
+        data = {}
+        
+        data['success'] = True
+        
+        return json.dumps(data)
         
     else:
         return react.unknown_method('/learn')
 
 
-@app.route('/guess')
+@app.route('/guess', methods=['GET', 'POST'])
 def guess():
     print "{} /guess".format(request.method)
 
     if request.method == 'GET':
         return react.guess_get()
+        
     elif request.method == 'POST':
-        return react.guess_post()
+        vector_dict = request.get_json()['vectors']
+        
+        urls = vector_dict.keys()
+        attr_vecs = []
+        
+        for url in urls:
+            attr_vec = deepcopy([float(i) for i in vector_dict[url]])
+            attr_vecs.append(attr_vec)
+            
+        img_classes, img_probas = react.guess_post(attr_vecs)
+        
+        data = {}
+        
+        try:
+            if len(urls) != len(img_classes):
+                raise MismatchUrlToClassException("Received %d urls but computed %d classes. Check classifier has sufficient data to predict." % (len(urls), len(img_classes)))              
+        except MismatchUrlToClassException as e:
+            print e
+            data["success"] = False
+            return json.dumps(data)
+        
+        for i in range (0, len(urls)):
+            data[str(urls[i])] = img_classes[i].item()
+            
+        data["success"] = True        
+        
+        return json.dumps(data)
+        
     else:
         return react.unknown_method('/guess')
+        
+        
+class MismatchUrlToClassException(Exception):
+        pass
 
 
 if __name__ == '__main__':
